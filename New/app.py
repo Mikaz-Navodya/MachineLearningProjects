c2160bc9a39f559d8b16b16b2edee87eca0bb698
Mikaz Navodya <mikaznavodya@gmail.com>
2025-09-18 11:21:06 +0530

    deleted unwanted files

--- New/app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import matplotlib.pyplot as plt
-import sklearn as sk
-import pandas as pd
-
-df = pd.read_csv("mushrooms.csv")
-st.write('Hello')
-l=sk.linear_model.LinearRegression()
-plt.figure()
