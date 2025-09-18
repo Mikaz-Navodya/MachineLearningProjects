da87cab6d303a867e81450cb80d26422d8941e82
Mikaz Navodya <mikaznavodya@gmail.com>
2025-09-18 10:00:05 +0530

    Update app.py

--- New/app.py
+++ New/app.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import sklearn as sk
 import pandas as pd
 
-df = pd.read_csv('mushrooms.csv')
+df = pd.read_csv("mushrooms.csv")
 st.write('Hello')
 l=sk.linear_model.LinearRegression()
 plt.figure()
